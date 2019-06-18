359744cb9c788d9ac1cbfa22f3fcfb19b3bd81df
diaojianfeng <466283062@qq.com>
2019-06-18 15:46:30 +0800

    update header.jsp

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 
 #需要配置如下参数
 # 项目路径, 在Execute Shell中配置项目路径, pwd 就可以获得该项目路径
-export PROJ_PATH=/root
+#export PROJ_PATH=/root
 
 # 输入你的环境上tomcat的全路径
-export TOMCAT_APP_PATH=/root/apache-tomcat-9.0.21
+#export TOMCAT_APP_PATH=/root/apache-tomcat-9.0.21
 
 ### base 函数
 killTomcat()
